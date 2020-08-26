012895d806bbe33322c461d12a11679971d6a531
Junchi <benson26365156@gmail.com>
2020-08-26 17:50:51 +0800

    修改關鍵字與寫法

--- controllers/LineController.js
+++ controllers/LineController.js
@@ -26,47 +26,67 @@ function downloadContent(messageId, downloadPath) {
 }
 
 class LineController {
-  async index(req, res) {
+  index(req, res) {
     if (req.body && req.body.events) {
       for (let event of req.body.events) {
         let userId = event.source.userId,
           replyToken = event.replyToken,
           sourceType = event.source.type;
 
-        if (event.type === 'message') {
-          let message = event.message;
-          let inputText, replyText;
-          switch (message.type) {
-            case 'text':
-              inputText = message.text;
+        new Promise(async (resolve, reject) => {
+          if (event.type === 'message') {
+            let message = event.message;
+            let inputText, replyText;
+            switch (message.type) {
+              case 'text':
+                inputText = message.text;
 
-              if (/(吸貓|貓貓|貓咪|喵咪|屁貓|笨貓|胖貓)/.test(inputText)) {
-                let image = await GooglePhotos.getImage();
+                if (/(貓|喵)/.test(inputText)) {
+                  let image = await GooglePhotos.getImage();
 
-                lineClient.replyMessage(replyToken, {
-                  type: 'image',
-                  originalContentUrl: image,
-                  previewImageUrl: image,
-                });
-              }
-              break;
-            case 'audio':
-              inputText = await STTAndTTS.saveLineAudioAndConvertToText(message.id);
+                  resolve([
+                    {
+                      type: 'image',
+                      originalContentUrl: image,
+                      previewImageUrl: image,
+                    },
+                  ]);
+                } else {
+                  reject();
+                }
+                break;
+              case 'audio':
+                inputText = await STTAndTTS.saveLineAudioAndConvertToText(message.id);
 
-              console.log('Google 聲音辨識為：', inputText);
-              replyText = await STTAndTTS.inputAndReplyContent(inputText);
-              console.log('回覆為：', replyText);
+                console.log('Google 聲音辨識為：', inputText);
+                replyText = await STTAndTTS.inputAndReplyContent(inputText);
+                console.log('回覆為：', replyText);
 
-              let lineAudioObject = await STTAndTTS.textConvertToAudioAndComposeLineAudioObject(replyText);
-              // lineClient.pushMessage(userId, lineAudioObject);
-              lineClient.replyMessage(replyToken, lineAudioObject);
-              break;
-            default:
-              console.log('Other Message', message);
-              break;
+                let lineAudioObject = await STTAndTTS.textConvertToAudioAndComposeLineAudioObject(replyText);
+                resolve([lineAudioObject]);
+                break;
+              default:
+                reject();
+                break;
+            }
+          } else {
+            reject();
           }
-        } else if (event.type === 'postback') {
-        }
+        })
+          .then((messages) => {
+            for (let index in messages) {
+              let message = messages[index];
+              if (index === '0') {
+                lineClient.replyMessage(replyToken, message);
+              } else {
+                lineClient.pushMessage(userId, message);
+              }
+            }
+          })
+          .catch(() => {
+            console.log('Other Event');
+            console.log(JSON.stringify(event, null, 2));
+          });
       }
       res.send();
     }
